f8e3227964400e3f4c96115faf5249f1e6eb5858
dwane richards <dwanerich@gmail.com>
2019-12-12 05:32:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,11 +18,13 @@ end
 
 def add_element_to_start_of_array(array, element)
   
-  arary.unshift(element)
+  array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
+  
+  arr
   
 end
 
